9d57e63f66836e573c55f9f98e3a2c57c7ce0d20
Mert kızıloğlu <mert_besiktas24@hotmail.com>
2022-01-11 12:40:06 +0300

    react-router-dom finish

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,7 +10,7 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'hats',
+                    title: 'Hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     linkUrl: 'shop/hats'
