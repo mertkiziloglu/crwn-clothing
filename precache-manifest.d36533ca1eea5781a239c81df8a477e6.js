734733ab7553dc7302c0a829a8395080fa44e2ac
Mert kızıloğlu <mert_besiktas24@hotmail.com>
2022-01-17 10:38:07 +0300

    Updates

---
+++ precache-manifest.d36533ca1eea5781a239c81df8a477e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ceccb51810f45c67b5f70f124d1e9d",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "5a5f9cd1af09d0d1a350",
+    "url": "/crwn-clothing/static/css/main.d6634b17.chunk.css"
+  },
+  {
+    "revision": "35e4c75088c317650570",
+    "url": "/crwn-clothing/static/js/2.4f2193db.chunk.js"
+  },
+  {
+    "revision": "5a5f9cd1af09d0d1a350",
+    "url": "/crwn-clothing/static/js/main.502b166b.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
